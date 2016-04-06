e57ea39c374290b3d3316e6279f0379dc0f6cc95
Andrew Lowe <doodlefolks@gmail.com>
2016-04-05 20:19:04 -0700

    stuff

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 (function() {
-  var trees = ['/Tree/bigTreeHD.jpg', '/Tree/blueTree.jpg', '/Tree/bonsaiTree.jpg', '/Tree/cherryTree.jpg', '/Tree/christmasTree.jpg', '/Tree/coconuteTree.jpg', '/Tree/csTree.jpg', '/Tree/deadTree.jpg', '/Tree/pineTree.jpg', '/Tree/treeOfLife.jpg'];
-  var notTrees = [];
+  var trees = ['Tree/bigTreeHD.jpg', 'Tree/blueTree.jpg', 'Tree/bonsaiTree.jpg', 'Tree/cherryTree.jpg', 'Tree/christmasTree.jpg', 'Tree/coconuteTree.jpg', 'Tree/csTree.jpg', 'Tree/deadTree.jpg', 'Tree/pineTree.jpg', 'Tree/treeOfLife.jpg'];
+  var notTrees = ['http://www.placecage.com/300/200'];
+  var score = 0;
 
   function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -13,21 +14,39 @@
   }
 
   $(document).ready(function () {
+    var newPics = loadPics();
+    $('.left').attr('src', newPics.tree);
+    $('.right').attr('src', newPics.notTree);
+
     $('img').click(function () {
       var picked = $(this);
       var notPicked = $(this).closest('img');
       var parentClass = $(this).parent().attr('class');
-      var parentIndex = parseInt(parentClass.split('set')[1]);
-      var nextSection = 'set' + (parentIndex + 1);
 
       if (trees.indexOf(picked.attr('src')) == -1) {
         // you lose
       } else {
-        // you win
+        score++;
+        $('#score').html(score);
       }
 
-      $(nextSection).attr('display', 'block');
-      $(this).parent().attr('display', 'none');
+      var newPics = loadPics();
+      if (Math.random() > 0.5) {
+        $('.left').attr('src', newPics.tree);
+        $('.right').attr('src', newPics.notTree);
+      } else {
+        $('.right').attr('src', newPics.tree);
+        $('.left').attr('src', newPics.notTree);
+      }
     });
+    window.setTimeout(function () {
+      var message;
+      if (score < 10) {
+        message = 'Not a tree hugger...';
+      }
+      alert('Your score:' score);
+      alert(message);
+      document.location.reload(true);
+    }, 10000);
   });
 }())
